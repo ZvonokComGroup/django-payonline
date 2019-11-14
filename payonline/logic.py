cd95fa459e9ba41d21c3e1f3737036e9c1415c1e
Ivan Petukhov <satels@gmail.com>
2019-11-14 13:27:42 +0300

    Update logic.py

--- payonline/logic.py
+++ payonline/logic.py
@@ -6,10 +6,10 @@ from .settings import CONFIG
 def get_security_key(order_id, amount, merchant_id=CONFIG['MERCHANT_ID'],
                      currency=CONFIG['CURRENCY'],
                      private_security_key=CONFIG['PRIVATE_SECURITY_KEY']):
-    params = [('MerchantId', merchant_id),
-              ('OrderId', order_id),
-              ('Amount', amount),
-              ('Currency', currency),
+    params = [('MerchantId', str(merchant_id)),
+              ('OrderId', str(order_id)),
+              ('Amount', str(amount)),
+              ('Currency', str(currency)),
               ('PrivateSecurityKey', private_security_key)]
 
     return md5('&'.join('='.join(i) for i in params)).hexdigest()
