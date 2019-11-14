88675641fc5f4312f41e79f64621fa2bac153aed
Ivan Petukhov <satels@gmail.com>
2019-11-14 13:41:37 +0300

    fix gen security key

--- payonline/logic.py
+++ payonline/logic.py
@@ -5,11 +5,16 @@ from .settings import CONFIG
 
 def get_security_key(order_id, amount, merchant_id=CONFIG['MERCHANT_ID'],
                      currency=CONFIG['CURRENCY'],
-                     private_security_key=CONFIG['PRIVATE_SECURITY_KEY']):
+                     private_security_key=CONFIG['PRIVATE_SECURITY_KEY'], valid_until='',
+                     order_description=''):
     params = [('MerchantId', str(merchant_id)),
               ('OrderId', str(order_id)),
               ('Amount', str(amount)),
-              ('Currency', str(currency)),
-              ('PrivateSecurityKey', private_security_key)]
+              ('Currency', str(currency))]
+    if valid_until:
+        params += [('ValidUntil', valid_until)]
+    if order_description:
+        params += [('OrderDescription', order_description)]
+    params += [('PrivateSecurityKey', private_security_key)]
 
     return md5('&'.join('='.join(i) for i in params)).hexdigest()
